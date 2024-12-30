c299fa12c11c1bfa16e0ff45e099a83a81045264
elroxx <charles.etienne.frechette@gmail.com>
2024-12-30 15:37:18 -0500

    Initial commit

---
+++ src/main/java/com/elementalconvergence/ElementalConvergenceClient.java
@@ -0,0 +1,11 @@
+package com.elementalconvergence;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalConvergenceClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
