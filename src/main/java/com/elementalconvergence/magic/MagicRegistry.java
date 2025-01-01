7707253371f9f404837a2291d839d6cb9fcb489b
ZacLafond <zachary.lafond@outlook.fr>
2025-01-01 17:05:23 -0500

    Add texture for eye of fire and magic eye

--- src/main/java/com/elementalconvergence/magic/MagicRegistry.java
+++ src/main/java/com/elementalconvergence/magic/MagicRegistry.java
@@ -7,6 +7,7 @@ public class MagicRegistry {
     private static final IMagicHandler[] MAGIC_HANDLERS = new IMagicHandler[ElementalConvergence.BASE_MAGIC_ID.length]; // Size matches your magic types
 
     public static void initialize() {
+        /*
         MAGIC_HANDLERS[0] = new EarthMagicHandler();
         MAGIC_HANDLERS[1] = new AirMagicHandler();
         MAGIC_HANDLERS[2] = new FireMagicHandler();
@@ -14,6 +15,7 @@ public class MagicRegistry {
         MAGIC_HANDLERS[4] = new ShadowMagicHandler();
         MAGIC_HANDLERS[5] = new LightMagicHandler();
         MAGIC_HANDLERS[6] = new LifeMagicHandler();
+         */
     }
 
     public static IMagicHandler getHandler(int magicIndex) {
