80ab32d265a1175c8e3302f0cef24fa50dfed598
elroxx <charles.etienne.frechette@gmail.com>
2025-01-01 21:10:03 -0500

    Add keybinds only clientside

--- src/main/java/com/elementalconvergence/ElementalConvergence.java
+++ src/main/java/com/elementalconvergence/ElementalConvergence.java
@@ -7,16 +7,22 @@ import com.elementalconvergence.magic.MagicRegistry;
 import com.elementalconvergence.magic.SpellManager;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 
 public class ElementalConvergence implements ModInitializer {
 	public static final String MOD_ID = "elemental-convergence";
@@ -24,6 +30,12 @@ public class ElementalConvergence implements ModInitializer {
 	public static final String[] BASE_MAGIC_DISPLAY = {"Earth", "Air", "Fire", "Water", "Shadow", "Light", "Life", "Death"};
 	public static final String[] BASE_MAGIC_ID = {"earth", "air", "fire", "water", "shadow", "light", "life", "death"};
 	public static final int TICKSEC = 20;
+
+
+	// Keybindings
+	private static KeyBinding primarySpellKb;
+	private static KeyBinding secondarySpellKb;
+	private static KeyBinding tertiarySpellKb;
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -36,9 +48,11 @@ public class ElementalConvergence implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
-		//Items initialization
-		ModItems.initialize();
-		MagicRegistry.initialize();
+
+		//Basic Initialization
+		ModItems.initialize(); //Items
+		MagicRegistry.initialize(); //Magic Types and spells
+		registerKeybindings(); //Keybinds
 
 		// COMMANDS SECTION
 		CommandRegistrationCallback.EVENT.register(SetMagicLevelCommand::register); //Registration of the SetMagicLevelCommand
@@ -55,7 +69,7 @@ public class ElementalConvergence implements ModInitializer {
 		});
 
 		//RIGHT CLICK
-		System.out.println("TESTING PRINT");
+		//System.out.println("TESTING PRINT");
 		UseItemCallback.EVENT.register((player, world, hand) -> {
 			if (!world.isClient()) {
 				SpellManager.handleRightClick(player);
@@ -70,6 +84,55 @@ public class ElementalConvergence implements ModInitializer {
 			}
 			return ActionResult.PASS;
 		});
+	}
+
+	private void registerKeybindings() {
+		//
+		primarySpellKb = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key." + MOD_ID + ".primary_spell",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_Z,
+				"category." + MOD_ID + ".spells"
+		));
+
+		secondarySpellKb = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key." + MOD_ID + ".secondary_spell",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_X,
+				"category." + MOD_ID + ".spells"
+		));
+
+		tertiarySpellKb = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key." + MOD_ID + ".tertiary_spell",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_C,
+				"category." + MOD_ID + ".spells"
+		));
+
+		// Register the tick event to check for key presses
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (primarySpellKb.wasPressed()) {
+				handleSpellKey(1);
+			}
+			if (secondarySpellKb.wasPressed()) {
+				handleSpellKey(2);
+			}
+			if (tertiarySpellKb.wasPressed()) {
+				handleSpellKey(3);
+			}
+		});
+	}
+
+	private void handleSpellKey(int spellNumber) {
+		LOGGER.info("Spell key " + spellNumber + " got pressed"); //FOR TESTING PURPOSES
+		//CALL MY GOATED SPELLMANAGER HERE
+		if (!(spellNumber==1 || spellNumber==2 || spellNumber==3)){
+			System.out.println("Error: Wrong spellkey number somehow?");
+		}
+		else{
+
+		}
+
 	}
 
 
