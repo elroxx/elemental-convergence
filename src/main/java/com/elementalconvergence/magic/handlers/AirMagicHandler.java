a8864839b9cb5953ef7db408f986dc6f68306d13
elroxx <charles.etienne.frechette@gmail.com>
2025-04-28 15:39:13 -0400

    Add Air Lvl1

--- src/main/java/com/elementalconvergence/magic/handlers/AirMagicHandler.java
+++ src/main/java/com/elementalconvergence/magic/handlers/AirMagicHandler.java
@@ -1,15 +1,75 @@
 package com.elementalconvergence.magic.handlers;
 
+import com.elementalconvergence.data.IMagicDataSaver;
+import com.elementalconvergence.data.MagicData;
 import com.elementalconvergence.magic.IMagicHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.WindChargeEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 public class AirMagicHandler implements IMagicHandler {
+
+    public static final int AIR_INDEX=1;
+
+    //For passive
+    public static final float AIR_SPEED = 0.18f;
+
+    public static final int DEFAULT_WINDBALL_COOLDOWN = 10; //0.25 seconds
+    private int windballCooldown = 0;
+
+
+
     @Override
     public void handleItemRightClick(PlayerEntity player) {
+
+        ItemStack mainHand = player.getMainHandStack();
+        ItemStack offHand = player.getOffHandStack();
+
+        IMagicDataSaver dataSaver = (IMagicDataSaver) player;
+        MagicData magicData = dataSaver.getMagicData();
+        int airLevel = magicData.getMagicLevel(AIR_INDEX);
+        if (airLevel>=1) {
+
+            if (windballCooldown == 0) {
+                if (mainHand.isOf(Items.FEATHER) || offHand.isOf(Items.FEATHER)) {
+
+                    if (!player.getAbilities().creativeMode) {
+                        if (mainHand.isOf(Items.FEATHER) || mainHand.isOf(Items.FEATHER)) {
+                            mainHand.decrement(1);
+                        } else {
+                            offHand.decrement(1);
+                        }
+                    }
+                    for (int i=0; i<2; i++) {
+                        double speed = 4.0;
+                        Vec3d rotation = player.getRotationVec(1.0F);
+                        Vec3d velocity = rotation.multiply(speed);
+
+                        WindChargeEntity windball = new WindChargeEntity(player.getWorld(), player.getX(), player.getY(), player.getZ(), velocity);
+
+                        windball.setVelocity(velocity);
+
+                        windball.setPosition(player.getX() + rotation.x * 1.5, player.getY() + 1.0, player.getZ() + rotation.z * 1.5);
+
+                        player.getWorld().spawnEntity(windball);
+
+                        player.getWorld().playSound(null, player.getX(), player.getY(), player.getZ(),
+                                SoundEvents.ENTITY_BREEZE_WIND_BURST, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    }
+
+                    windballCooldown=DEFAULT_WINDBALL_COOLDOWN;
+                }
+            }
+        }
+
     }
 
     @Override
@@ -19,6 +79,19 @@ public class AirMagicHandler implements IMagicHandler {
 
     @Override
     public void handlePassive(PlayerEntity player) {
+
+        //Buff (movement speed+ 1/10 gravity)
+
+
+
+        if (windballCooldown%6==1) {
+        }
+
+        //Cooldowns
+
+        if (windballCooldown>0){
+            windballCooldown--;
+        }
 
     }
 
