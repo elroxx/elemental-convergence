9f6cc19b36764a5bd54e7f09ddf6b4137da50540
elroxx <charles.etienne.frechette@gmail.com>
2025-05-01 20:16:25 -0400

    Add water lvl 1

--- src/main/java/com/elementalconvergence/magic/handlers/WaterMagicHandler.java
+++ src/main/java/com/elementalconvergence/magic/handlers/WaterMagicHandler.java
@@ -6,14 +6,28 @@ import com.elementalconvergence.effect.ModEffects;
 import com.elementalconvergence.magic.IMagicHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import virtuoel.pehkui.api.PehkuiConfig;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WaterMagicHandler implements IMagicHandler {
 
@@ -21,8 +35,81 @@ public class WaterMagicHandler implements IMagicHandler {
 
     private boolean toggleDolphin = false;
 
+    public void onNautilusEnchant(PlayerEntity player, ItemStack offHand){
+        offHand.decrement(1);
+
+        player.getWorld().playSound(
+                null,
+                player.getX(), player.getY(), player.getZ(),
+                SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE,
+                SoundCategory.PLAYERS,
+                1.0F,
+                1.0F
+        );
+    }
+
     @Override
     public void handleItemRightClick(PlayerEntity player) {
+        ItemStack mainHand = player.getMainHandStack();
+        ItemStack offHand = player.getOffHandStack();
+
+        IMagicDataSaver dataSaver = (IMagicDataSaver) player;
+        MagicData magicData = dataSaver.getMagicData();
+        int waterLevel = magicData.getMagicLevel(WATER_INDEX);
+        ServerWorld world = (ServerWorld) player.getWorld();
+
+        if (waterLevel>=1){
+            if (offHand.isOf(Items.PRISMARINE_CRYSTALS)){
+                if (mainHand.isOf(Items.TRIDENT)){
+                    RegistryEntry<Enchantment> impaleEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.IMPALING);
+                    int impaleLvl = EnchantmentHelper.getLevel(impaleEntry, mainHand);
+
+                    if (impaleLvl<5){
+                        mainHand.addEnchantment(impaleEntry, 5);
+                        onNautilusEnchant(player, offHand);
+                    }
+                }
+
+                if (mainHand.isOf(Items.DIAMOND_BOOTS) || mainHand.isOf(Items.GOLDEN_BOOTS) || mainHand.isOf(Items.NETHERITE_BOOTS) || mainHand.isOf(Items.IRON_BOOTS) || mainHand.isOf(Items.CHAINMAIL_BOOTS) || mainHand.isOf(Items.LEATHER_BOOTS)){
+                    RegistryEntry<Enchantment> depthEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.DEPTH_STRIDER);
+                    int depthLvl = EnchantmentHelper.getLevel(depthEntry, mainHand);
+
+                    if (depthLvl<3){
+                        mainHand.addEnchantment(depthEntry, 3);
+                        onNautilusEnchant(player, offHand);
+                    }
+                }
+
+                if (mainHand.isOf(Items.DIAMOND_HELMET) || mainHand.isOf(Items.GOLDEN_HELMET) || mainHand.isOf(Items.NETHERITE_HELMET) || mainHand.isOf(Items.IRON_HELMET) || mainHand.isOf(Items.CHAINMAIL_HELMET) || mainHand.isOf(Items.LEATHER_HELMET) || mainHand.isOf(Items.TURTLE_HELMET)){
+                    RegistryEntry<Enchantment> respirationEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.RESPIRATION);
+                    int respirationLvl = EnchantmentHelper.getLevel(respirationEntry, mainHand);
+                    RegistryEntry<Enchantment> affinityEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.AQUA_AFFINITY);
+                    int affinityLvl = EnchantmentHelper.getLevel(affinityEntry, mainHand);
+
+                    if (respirationLvl<3 || affinityLvl<1){
+                        mainHand.addEnchantment(respirationEntry, 3);
+                        mainHand.addEnchantment(affinityEntry, 1);
+                        onNautilusEnchant(player, offHand);
+                    }
+                }
+
+                if (mainHand.isOf(Items.FISHING_ROD)){
+                    RegistryEntry<Enchantment> luckEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.LUCK_OF_THE_SEA);
+                    int luckLvl = EnchantmentHelper.getLevel(luckEntry, mainHand);
+
+                    RegistryEntry<Enchantment> lureEntry = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.LURE);
+                    int lureLvl = EnchantmentHelper.getLevel(luckEntry, mainHand);
+
+                    if (lureLvl<3 || luckLvl<3){
+                        mainHand.addEnchantment(lureEntry, 3);
+                        mainHand.addEnchantment(luckEntry, 3);
+                        onNautilusEnchant(player, offHand);
+                    }
+
+
+                }
+            }
+        }
 
     }
 
@@ -84,6 +171,16 @@ public class WaterMagicHandler implements IMagicHandler {
 
         if (waterLevel>=2){
             toggleDolphin=!toggleDolphin;
+
+            // sound
+            player.getWorld().playSound(
+                    null,
+                    player.getX(), player.getY(), player.getZ(),
+                    SoundEvents.ENTITY_DOLPHIN_SWIM,
+                    SoundCategory.PLAYERS,
+                    1.0F,
+                    1F
+            );
         }
 
     }
