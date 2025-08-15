991e095f0e4060934e6711a457f9fa1e2ae147e2
elroxx <charles.etienne.frechette@gmail.com>
2025-08-15 15:40:38 -0400

    Add sound and particles for portable beehive. Missing block and nbt stored position maybe

--- src/main/java/com/elementalconvergence/item/PortableBeehiveItem.java
+++ src/main/java/com/elementalconvergence/item/PortableBeehiveItem.java
@@ -6,7 +6,10 @@ import com.elementalconvergence.data.MagicData;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -28,6 +31,24 @@ public class PortableBeehiveItem extends Item {
         ItemStack itemStack = user.getStackInHand(hand);
 
         if (!world.isClient && user instanceof ServerPlayerEntity serverPlayer) {
+
+            serverPlayer.getServerWorld().playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.BLOCK_HONEY_BLOCK_PLACE, SoundCategory.PLAYERS,1.0f, 0.01f);
+
+            for (int i = 0; i < 10; i++) {
+                double offsetX = world.random.nextGaussian() * 0.1;
+                double offsetY = world.random.nextGaussian() * 0.1;
+                double offsetZ = world.random.nextGaussian() * 0.1;
+
+                serverPlayer.getServerWorld().spawnParticles(
+                        ParticleTypes.FALLING_HONEY,
+                        serverPlayer.getX(), serverPlayer.getY() + 0.5, serverPlayer.getZ(),
+                        10,
+                        offsetX, offsetY, offsetZ,
+                        1.0f
+                );
+            }
+
             if (serverPlayer.getServerWorld().getRegistryKey().equals(BEEHIVE_WORLD_KEY)) {
                 BeehiveTeleporter.exitBeehive(serverPlayer);
             } else {
@@ -42,6 +63,8 @@ public class PortableBeehiveItem extends Item {
             }
 
             //playsound + particles hopefully
+            serverPlayer.getServerWorld().playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.BLOCK_HONEY_BLOCK_PLACE, SoundCategory.PLAYERS,1.0f, 0.01f);
         }
 
         return TypedActionResult.success(itemStack, world.isClient());
