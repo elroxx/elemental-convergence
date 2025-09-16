b02c789ff77a1f043b7c94dbb906f2c0477531a5
elroxx <charles.etienne.frechette@gmail.com>
2025-09-16 14:08:36 -0400

    Add debuff

--- src/main/java/com/elementalconvergence/magic/convergencehandlers/VoidMagicHandler.java
+++ src/main/java/com/elementalconvergence/magic/convergencehandlers/VoidMagicHandler.java
@@ -9,15 +9,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleTypes;
 
@@ -26,7 +31,18 @@ import static com.elementalconvergence.ElementalConvergence.BASE_MAGIC_ID;
 public class VoidMagicHandler implements IMagicHandler {
     public static final int VOID_INDEX= (BASE_MAGIC_ID.length-1)+10;
 
+    public static final int DEFAULT_DIMENSIONSICK_COOLDOWN = 10;
+    private int dimensionSickCooldown=0;
+
     public static final int DEFAULT_VOID_DRILL_COOLDOWN = 20*10; //10 seconds
+
+    public static final int DEFAULT_VOID_SWAP_COOLDOWN = 20;
+    private int voidSwapCooldown=0;
+    public static final RegistryKey<World> VOID_DIMENSION = World.END; //to change once i have the void dimension done
+    //void dimension needs to go from 0 to 96 because i want to do *4 everywhere (and its 384 blocks in the overworld.
+
+    //DONT TAKE DAMAGE IN END OR IN VOID
+    //CANT USE THE ABILITY IN THE NETHER, ONLY OVERWORLD
 
 
     @Override
@@ -42,6 +58,30 @@ public class VoidMagicHandler implements IMagicHandler {
     @Override
     public void handlePassive(PlayerEntity player) {
 
+        //debuff dimension sickness
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+        RegistryKey<World> currentWorld = serverPlayer.getServerWorld().getRegistryKey();
+        if (currentWorld.equals(World.OVERWORLD) || currentWorld.equals(World.NETHER)) {
+
+            if (dimensionSickCooldown == 0) {
+                float hpLeft = player.getHealth()-1.0f;
+                if (hpLeft>=2.0f) { //so if one heart is left
+                    DamageSource withered = player.getWorld().getDamageSources().wither();
+                    player.damage(withered, 1.0f);
+                }
+                dimensionSickCooldown = DEFAULT_DIMENSIONSICK_COOLDOWN;
+            } else {
+                dimensionSickCooldown--;
+            }
+        }
+        else{
+            dimensionSickCooldown=DEFAULT_DIMENSIONSICK_COOLDOWN;
+        }
+
+        //cooldowns
+        if (voidSwapCooldown>0){
+            voidSwapCooldown--;
+        }
     }
 
     @Override
@@ -66,7 +106,11 @@ public class VoidMagicHandler implements IMagicHandler {
 
     @Override
     public void handlePrimarySpell(PlayerEntity player) {
+        //dimension swap:
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+        if (serverPlayer.getServerWorld().getRegistryKey().equals(VOID_DIMENSION)){
 
+        }
     }
 
     @Override
