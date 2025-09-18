7cd557a6cfb8d4ce95dfb9e4c3718ea4d187c95b
elroxx <charles.etienne.frechette@gmail.com>
2025-09-18 14:48:24 -0400

    add description for spider

--- src/main/java/com/elementalconvergence/magic/convergencehandlers/SpiderMagicHandler.java
+++ src/main/java/com/elementalconvergence/magic/convergencehandlers/SpiderMagicHandler.java
@@ -34,6 +34,29 @@ import static com.elementalconvergence.world.dimension.ModDimensions.VOID_WORLD_
 public class SpiderMagicHandler implements IMagicHandler {
     public static final int SPIDER_INDEX= (BASE_MAGIC_ID.length-1)+11;
 
+    public static final int SPIDER_LIGHT_THRESHOLD = 9;
+
+    //buff: wall climb
+    //debuff: can't attack in a light level that is too high (probably can attack up to when light level is 9. After 9, can't attack)
+    //buff normal zombies, skeleton, creeper and spider+cave spiders dont attack you. (MAYBE NOT??)
+    //Passive: Spider webs are solid blocks.
+
+    //lvl 1: When right clicking with a stack of string in the air. Create a line of cobwebs in the sky. Consume 1 string per block placed. Place them 1 by 1 with a small delay like the vein miner ability.
+    //lvl 2: web slinging
+    //lvl 3: when right clicking with a spider's abdomen, stun everybody in front in a cone. This gives them weakness 2 for like 10 seconds, slowness 2 for 10 seconds, poison 1 for 5 seconds and places a cobweb on their face and feet. 15 seconds cooldown.
+
+    //advancements:
+    //1: cobweb
+    //2: spider's abdomen (reusable item that will be used for the next ability)
+    //3: danger pottery shard
+
+    //spider's abdomen is like b:blackwoold c:scaffolding, l:loom
+    //bcb
+    //bLb
+    //bcb
+
+
+
     @Override
     public void handleItemRightClick(PlayerEntity player) {
 
